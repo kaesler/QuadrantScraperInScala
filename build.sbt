08aab4bc778f4ff2904c0f9462a060561bd3a329
Kevin Esler <kevin.esler@gmail.com>
2021-04-09 12:29:02 -0400

    sttp to build.

--- build.sbt
+++ build.sbt
@@ -8,5 +8,8 @@ lazy val root = project
 
     scalaVersion := scala3Version,
 
-    libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test"
+    libraryDependencies ++= Seq(
+      "com.softwaremill.sttp.client3" %% "core" % "3.3.0-RC1",
+      "com.novocode" % "junit-interface" % "0.11" % "test"
+    )
   )
--- src/main/scala/org/kae/quadrantscraper/Main.scala
+++ src/main/scala/org/kae/quadrantscraper/Main.scala
@@ -1,7 +1,20 @@
 @main
 def hello: Unit = {
-  println("Hello world!")
-  println(msg)
-}
+  import sttp.client3._
+  val sort: Option[String] = None
+  val query = "http language:scala"
 
-def msg = "I was compiled by Scala 3. :)"
+  // the `query` parameter is automatically url-encoded
+  // `sort` is removed, as the value is not defined
+  val request = basicRequest.get(uri"https://api.github.com/search/repositories?q=$query&sort=$sort")
+
+  val backend = HttpURLConnectionBackend()
+  val response = request.send(backend)
+
+  // response.header(...): Option[String]
+  println(response.header("Content-Length"))
+
+  // response.body: by default read into an Either[String, String] to
+  // indicate failure or success
+  println(response.body)
+}
